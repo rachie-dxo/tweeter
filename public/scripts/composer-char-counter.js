22ba63cd7029753d846f8ee2b38d4e6d86e0e13a
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-01-30 18:27:20 +0000

    Counter now counts backwards from limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,9 @@
 $( document ).ready(function() {
   $("textarea").on('keyup', function () {
     let input = $('#tweet-input')[0].value;
-    let textlength = input.length - 1;
-    textlength += 1;
-    $('.counter').text(textlength);
+    let max = 140;
+    let textlength = input.length;
+    max -= textlength;
+    $('.counter').text(max);
    });
 });
