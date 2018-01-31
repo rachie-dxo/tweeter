7a34c182929d56da861d5f51d5bab38cb6bf9026
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-01-31 17:05:07 +0000

    Added function to create tweet element

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,8 +4,54 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$( document ).ready(function() {
+  console.log("ayo");
+  function createTweetElement (tweetObj) {
 
-// $.getJSON("initial-tweets.json", function(data) {
-// console.log(data);
-// $('.tweets').append(data.content[0].text);
-//     });
+    var $tweet = $("<article>").addClass("tweet");
+
+    $("h1").text(tweetObj['user'].name);
+
+    $("h2").text(tweetObj["user"].handle);
+
+    $(".icon").text(tweetObj["user"].avatars.small);
+
+    $(".aTweet").text(tweetObj["content"].text);
+
+    $("footer").text(tweetObj.created_at);
+
+    $('#tweets').append($tweet);
+
+    return $tweet;
+
+  }
+
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+var $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+console.log(tweetData["user"].name);
+console.log(tweetData["user"].handle);
+console.log(tweetData["user"].avatars.small);
+console.log(tweetData["content"].text);
+console.log(tweetData.created_at);
+});
